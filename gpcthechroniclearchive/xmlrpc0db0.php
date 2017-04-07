59c142ffcfb0e792d81b0814ab28c0d82eefaccc
gastate <pjoseph7@gsu.edu>
2017-04-07 11:31:31 -0400

    replaced all occurences of thechonicle.gpc.edu with /gpcthechroniclearchive/

--- gpcthechroniclearchive/xmlrpc0db0.php
+++ gpcthechroniclearchive/xmlrpc0db0.php
@@ -5,10 +5,10 @@
     <engineLink>http://wordpress.org/</engineLink>
     <homePageLink>http://thechronicle.gpc.edu</homePageLink>
     <apis>
-      <api name="WordPress" blogID="1" preferred="true" apiLink="http://thechronicle.gpc.edu/xmlrpc.php" />
-      <api name="Movable Type" blogID="1" preferred="false" apiLink="http://thechronicle.gpc.edu/xmlrpc.php" />
-      <api name="MetaWeblog" blogID="1" preferred="false" apiLink="http://thechronicle.gpc.edu/xmlrpc.php" />
-      <api name="Blogger" blogID="1" preferred="false" apiLink="http://thechronicle.gpc.edu/xmlrpc.php" />
+      <api name="WordPress" blogID="1" preferred="true" apiLink="/gpcthechroniclearchive/xmlrpc.php" />
+      <api name="Movable Type" blogID="1" preferred="false" apiLink="/gpcthechroniclearchive/xmlrpc.php" />
+      <api name="MetaWeblog" blogID="1" preferred="false" apiLink="/gpcthechroniclearchive/xmlrpc.php" />
+      <api name="Blogger" blogID="1" preferred="false" apiLink="/gpcthechroniclearchive/xmlrpc.php" />
           </apis>
   </service>
 </rsd>
