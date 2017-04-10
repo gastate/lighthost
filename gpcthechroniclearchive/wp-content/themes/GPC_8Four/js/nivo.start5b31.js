00133e0b01c165d1dcdfcfa507f79861b780be0b
Pam Joseph <pamjoseph@gsu.edu>
2017-04-10 15:04:51 -0400

    url updates

---
+++ gpcthechroniclearchive/wp-content/themes/GPC_8Four/js/nivo.start5b31.js
@@ -0,0 +1,4 @@
+    $(window).load(function() {
+        $('#slider').nivoSlider({pauseTime: 8000, // How long each slide will show
+		});
+    });
---
+++ gpcthechroniclearchive/xmlrpc0db0.php
@@ -0,0 +1,14 @@
+
+<?xml version="1.0" encoding="UTF-8"?><rsd version="1.0" xmlns="http://archipelago.phrasewise.com/rsd">
+  <service>
+    <engineName>WordPress</engineName>
+    <engineLink>http://wordpress.org/</engineLink>
+    <homePageLink>http://thechronicle.gpc.edu</homePageLink>
+    <apis>
+      <api name="WordPress" blogID="1" preferred="true" apiLink="/gpcthechroniclearchive/xmlrpc.php" />
+      <api name="Movable Type" blogID="1" preferred="false" apiLink="/gpcthechroniclearchive/xmlrpc.php" />
+      <api name="MetaWeblog" blogID="1" preferred="false" apiLink="/gpcthechroniclearchive/xmlrpc.php" />
+      <api name="Blogger" blogID="1" preferred="false" apiLink="/gpcthechroniclearchive/xmlrpc.php" />
+          </apis>
+  </service>
+</rsd>
